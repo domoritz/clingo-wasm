6952193c5d3452b93b850bff6e69fabc94153efd
Daniel Hines <d4hines@gmail.com>
2019-09-05 18:48:28 -0400

    seperate into seperate release file

---
+++ release.sh
@@ -0,0 +1,12 @@
+if [[$CI]]; then
+  message=$(git show -s)
+  regexp="\\[Travis\\]"
+  if [[ $message =~ $regexp ]]; then
+    echo "Skipping rebuild of automated commit."
+  else
+    if [[ $TRAVIS_BRANCH != 'master' ]]; then
+      git add clingo.wasm clingo.js
+      git commit -m "[Travis] Automated build of WASM files (build $TRAVIS_BUILD_NUMBER)."
+    fi
+  fi
+fi
