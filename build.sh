b682dc5705e2563f6456600e27b8575bba9b9ea9
Daniel Hines <d4hines@gmail.com>
2019-09-05 18:45:39 -0400

    fix travis syntax error

--- build.sh
+++ build.sh
@@ -44,3 +44,16 @@ make -C build/web web
 # Copy the results to root.
 cd ..  
 cp ./$clingo/build/web/bin/clingo.* ./
+
+if [[$CI]]; then
+  message=$(git show -s)
+  regexp="\\[Travis\\]"
+  if [[ $message =~ $regexp ]]; then
+    echo "Skipping rebuild of automated commit."
+  else
+    if [[ $TRAVIS_BRANCH != 'master' ]]; then
+      git add clingo.wasm clingo.js
+      git commit -m "[Travis] Automated build of WASM files (build $TRAVIS_BUILD_NUMBER)."
+    fi
+  fi
+fi
