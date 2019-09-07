32597d5f2c7bcf3d6ef2dc5defce6faf674e0c24
Dominik Moritz <domoritz@gmail.com>
2019-09-07 14:58:00 -0700

    Update build script and add secret

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,5 @@
 FROM trzeci/emscripten
-WORKDIR /deps
-RUN apt-get update \
-&& apt-get upgrade -y \
-&& apt-get install bison re2c -y \
-# Fetch and compile Lua
-&& lua=lua-5.3.4 \
-&& wget https://www.lua.org/ftp/$lua.tar.gz \
-&& tar -xf $lua.tar.gz \
-&& mv $lua /deps/lua \
-&& cd /deps/lua \
-&& emmake make generic local CC='emcc -s WASM=1'
+RUN apt update \
+&& apt upgrade -y \
+&& apt install bison re2c -y
 WORKDIR /src
--- build.sh
+++ build.sh
@@ -2,21 +2,31 @@
 set -e
 set -x
 
+# Fetch and compile Lua.
+
+lua=lua-5.3.4
+wget https://www.lua.org/ftp/$lua.tar.gz -O lua.tar.gz
+tar -xf lua.tar.gz
+pushd $lua
+emmake make generic local
+popd
+
 # Fetch and compile Clingo.
+
 clingo_version=5.4.0
 clingo=clingo-$clingo_version
 wget https://github.com/potassco/clingo/archive/v$clingo_version.tar.gz -O clingo.tar.gz
 tar -xf clingo.tar.gz
 
-cd $clingo
+pushd $clingo
 mkdir -p build/web
-cd build/web
+pushd build/web
 
 emcmake cmake \
         -DCLINGO_BUILD_WEB=On \
         -DCLINGO_BUILD_WITH_PYTHON=Off \
-        -DLUA_INCLUDE_DIR=/deps/lua/install/include \
-        -DLUA_LIBRARIES=/deps/lua/install/lib/liblua.a \
+        -DLUA_INCLUDE_DIR=../../../$lua/install/include \
+        -DLUA_LIBRARIES=../../../$lua/install/lib/liblua.a \
         -DCLINGO_BUILD_WITH_LUA=On \
         -DCLINGO_REQUIRE_LUA=On \
         -DCLINGO_BUILD_SHARED=Off \
@@ -28,9 +38,9 @@ emcmake cmake \
         -DCMAKE_EXE_LINKER_FLAGS_RELEASE="" \
         ../..
 
-cd ../..
+popd
 make -C build/web web
 
 # Copy the results to root.
-cd ..  
-cp ./$clingo/build/web/bin/clingo.* ./
+popd  
+cp $clingo/build/web/bin/clingo.* ./
