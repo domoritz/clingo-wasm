0428fb668b0614a93d72977a84c702d28232a8c6
Daniel Hines <d4hines@gmail.com>
2019-09-09 08:46:57 -0400

    set git config add -x back to bash script
    I was worried about printing secrets out to console,
    but I see now that they're encrypted, and also that
    this script doesn't have any secrets

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,6 +5,9 @@ if [[ $(git status --porcelain | wc -l) -gt 0 ]]; then
     echo "Skipping rebuild of automated commit."
   else
     if [[ $TRAVIS_BRANCH != 'master' ]]; then
+      git config --global user.email "travis@travis-ci.org"
+      git config --global user.name "Travis CI"
+
       git add clingo.wasm clingo.js
       git commit -m "[Travis] Automated build of WASM files (build $TRAVIS_BUILD_NUMBER)."
       git remote add origin-pushable https://${GH_TOKEN}@github.com/domoritz/clingo-wasm.git > /dev/null 2>&1
