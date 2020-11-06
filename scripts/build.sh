e1858f9d19570958781b7b83e49942d2ccf224b2
Dominik Moritz <domoritz@gmail.com>
2020-11-06 10:57:42 -0800

    fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ set -ex
 
 source ${BASH_SOURCE%/*}/versions.sh
 
-docker run -it -v $(pwd):/src trzeci/emscripten:${emscripten_version}-upstream bash -c 'scripts/install_deps.sh; scripts/build_clingo.sh'
+docker run -v $(pwd):/src trzeci/emscripten:${emscripten_version}-upstream bash -c 'scripts/install_deps.sh; scripts/build_clingo.sh'
