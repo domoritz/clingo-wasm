4e389e605bd4361d054e86210bc59661dcc8f885
Dominik Moritz <domoritz@gmail.com>
2021-03-20 22:21:11 -0700

    Upgrade to Lua 5.3.6

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -2,7 +2,7 @@
 emscripten_version=2.0.15
 
 # Find latest version at https://github.com/lua/lua/releases
-lua_version=5.3.5
+lua_version=5.3.6
 
 # Find latest version at https://github.com/potassco/clingo/releases
 clingo_version=5.4.1
