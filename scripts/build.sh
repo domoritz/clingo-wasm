4578bb9a7786590d2515f88c49dc2793e8cbd481
Dominik Moritz <domoritz@gmail.com>
2021-03-20 22:02:04 -0700

    Use version from sourced file

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,6 @@
 
 set -ex
 
-docker run -v $(pwd):/src emscripten/emsdk:2.0.15 bash -c 'scripts/install_deps.sh; scripts/build_clingo.sh'
+source ${BASH_SOURCE%/*}/versions.sh
+
+docker run -v $(pwd):/src emscripten/emsdk:${emscripten_version} bash -c 'scripts/install_deps.sh; scripts/build_clingo.sh'
