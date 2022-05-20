416fd799ef4cbc0ce03e39259066ab6ddd883c68
Dominik Moritz <domoritz@gmail.com>
2022-05-20 14:48:28 -0700

    feat: upgrade clingo and emscripten (#203)
    Co-authored-by: GitHub Action <domoritz@users.noreply.github.com>

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,8 +1,8 @@
 # Find latest version at https://hub.docker.com/r/emscripten/emsdk/tags
-emscripten_version=3.0.0
+emscripten_version=3.1.10
 
 # Find latest version at https://github.com/lua/lua/releases
 lua_version=5.3.6
 
 # Find latest version at https://github.com/potassco/clingo/releases
-clingo_version=5.5.1
+clingo_version=5.5.2
