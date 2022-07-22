3354a20bc2032ccb4262ab564a97aad5d96186a6
Péter Ferenc Gyarmati <40776291+peter-gy@users.noreply.github.com>
2022-07-22 16:44:16 +0200

    fix: quote bash variables to avoid word splitting (#226)

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-source ${BASH_SOURCE%/*}/versions.sh
+source "${BASH_SOURCE%/*}"/versions.sh
 
-docker run -v $(pwd):/src emscripten/emsdk:${emscripten_version} bash -c 'scripts/install_deps.sh; scripts/build_clingo.sh'
+docker run -v "$(pwd)":/src emscripten/emsdk:${emscripten_version} bash -c 'scripts/install_deps.sh; scripts/build_clingo.sh'
